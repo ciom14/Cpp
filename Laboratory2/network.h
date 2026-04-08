4df09e1da8db7b9f23c3c0ac1af412c5b0ea909a
ciom14 <freadbear1402@gmail.com>
2026-04-08 16:19:51 +0300

    Add files via upload

---
+++ Laboratory2/network.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "layer.h"
+#include <vector>
+
+class Network {
+public:
+    std::vector<Layer> layers;
+
+    Network(std::vector<int> layerSizes, int inputSize);
+    Matrix forward(const Matrix& input) const;
+    double mse(double predicted, double expected) const;
+    void train(const Matrix& input, double expected, double learningRate);
+};
