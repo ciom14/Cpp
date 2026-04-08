9eecfb26c89199c6725e51377e5b83c63045d831
ciom14 <freadbear1402@gmail.com>
2026-04-08 16:23:20 +0300

    Add files via upload

---
+++ Laboratory2/Laboratory2/layer.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include "neuron.h"
+#include <vector>
+
+class Layer {
+public:
+    std::vector<Neuron> neurons;
+
+    Layer(int neuronCount, int inputSize);
+    Matrix forward(const Matrix& input) const;
+};
