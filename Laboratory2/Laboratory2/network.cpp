6c4cdb549df9fca64bf15b3b10809721b85e714d
ciom14 <freadbear1402@gmail.com>
2026-04-22 16:22:39 +0300

    Add files via upload

---
+++ Laboratory2/Laboratory2/network.cpp
@@ -0,0 +1,35 @@
+﻿#include "network.h"
+#include "utils.h"
+
+Network::Network(std::vector<int> layerSizes, int inputSize) {
+    int currentInputSize = inputSize;
+    for (int i = 0; i < layerSizes.size(); i++) {
+        layers.push_back(Layer(layerSizes[i], currentInputSize));
+        currentInputSize = layerSizes[i];
+    }
+}
+
+Matrix Network::forward(Matrix input) const {
+    for (int i = 0; i < layers.size(); i++)
+        input = layers[i].forward(input);
+    return input;
+}
+
+double Network::mse(double predicted, double expected) const {
+    double error = predicted - expected;
+    return error * error;
+}
+
+void Network::train(const Matrix& input, double expected, double learningRate) {
+    for (int l = 0; l < layers.size(); l++)
+        for (int n = 0; n < layers[l].neurons.size(); n++) {
+            for (int w = 0; w < layers[l].neurons[n].weights.cols; w++)
+                updateParameter(layers[l].neurons[n].weights.data[0][w], [&]() {
+                return mse(forward(input).data[0][0], expected);
+                    }, learningRate);
+
+            updateParameter(layers[l].neurons[n].bias, [&]() {
+                return mse(forward(input).data[0][0], expected);
+                }, learningRate);
+        }
+}
